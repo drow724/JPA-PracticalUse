d21bc572c2c873dae6861eac4fe11de974296ddd
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-11-18 01:13:51 +0900

    변경 감지와 병합(merge)

--- src/main/java/jpabook/jpashop/domain/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/domain/controller/ItemController.java
@@ -65,17 +65,18 @@ public class ItemController {
 	}
 	
 	@PostMapping("items/{itemId}/edit")
-	public String updateItem(@ModelAttribute("form") BookForm form) {
+	public String updateItem(@PathVariable("itemId") Long itemId, @ModelAttribute("form") BookForm form) {
+
+		//컨트롤러에서 어설프게 엔티티 생성하지 말자
+//		Book book = new Book();
+//		book.setId(form.getId());
+//		book.setName(form.getName());
+//		book.setPrice(form.getPrice());
+//		book.setStockQuantity(form.getStockQuantity());
+//		book.setAuthor(form.getAuthor());
+//		book.setIsbn(form.getIsbn());
 		
-		Book book = new Book();
-		book.setId(form.getId());
-		book.setName(form.getName());
-		book.setPrice(form.getPrice());
-		book.setStockQuantity(form.getStockQuantity());
-		book.setAuthor(form.getAuthor());
-		book.setIsbn(form.getIsbn());
-		
-		itemService.saveItem(book);
+		itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
 		return "redirect:/items";
 	}
 }
--- src/main/java/jpabook/jpashop/domain/service/ItemService.java
+++ src/main/java/jpabook/jpashop/domain/service/ItemService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.domain.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,21 @@ public class ItemService {
 	public void saveItem(Item item) {
 		itemRepository.save(item);
 	}
+	
+	@Transactional
+	public void updateItem(Long itemId, String name, int price, int stockQunatity) { // itemParam: 파리미터로 넘어온 준영속 상태의 엔티티
+		Item findItem = itemRepository.findOne(itemId); // 같은 엔티티를 조회한다.
+		//findItem.change(name, price, stockQunatity); //의미 있는 메소드를 만들어서 변경하자
+		findItem.setName(name);// 데이터를 수정한다.
+		findItem.setPrice(price); 
+		findItem.setStockQuantity(stockQunatity); 
+		//itemRepository.save(findItem); //변경 감지 기능 덕분에 실행할 필요 X
+	}
+	
+//	@Transactional
+//	void update(Item itemParam) { //itemParam: 파리미터로 넘어온 준영속 상태의 엔티티
+//		Item mergeItem = em.merge(itemParam); //null값이 올 위험이 있어서 실무에서 쓰지 않음!!
+//	}
 	
 	public List<Item> findItems() {
 		return itemRepository.findAll();
