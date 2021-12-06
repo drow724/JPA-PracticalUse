159ba21f17cf3c83e8a29322fe13bfcb81f01e8d
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-12-06 22:56:55 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 /*
@@ -32,6 +38,38 @@ public class OrderSimpleApiController {
 			order.getDelivery().getAddress();
 		}
 		return all;
+	}
+	
+	@GetMapping("/api/v2/simple-orders")
+	public List<SimpleOrderDto> ordersV2() {
+		//ORDER 2개
+		//N + 1 -> 1 + 회원 N + 배송 N
+		List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+		
+		//2개
+		List<SimpleOrderDto> result = orders.stream().map(o -> new SimpleOrderDto(o))
+		.collect(Collectors.toList());
+		
+		return result;
+	}
+	
+	@Data
+	static class SimpleOrderDto {
+		
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+		
+		public SimpleOrderDto(Order order) {
+			orderId = order.getId();
+			name = order.getMember().getName(); //LAZY 초기화
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress(); //LAZY 초기화
+		}
+		
 	}
 	
 }
