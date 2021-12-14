20ed66b88628c1c103220da19f8fa1582bc9a36c
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-12-15 00:30:41 +0900

    주문 조회 V4: JPA에서 DTO직접 조회

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -15,6 +15,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -36,6 +38,8 @@ public class OrderApiController {
 	
 	private final OrderRepository orderRepository;
 
+	private final OrderQueryRepository orderQueryRepository;
+	
 	/**
 	 * V1. 엔티티 직접 노출
 	 * - Hibernate5Module 모듈 등록, LAZY=null 처리
@@ -81,6 +85,11 @@ public class OrderApiController {
 		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 		List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(toList());
 		return result;
+	}
+	
+	@GetMapping("/api/v4/orders")
+	public List<OrderQueryDto> ordersV4() {
+		return orderQueryRepository.findOrderQueryDtos();
 	}
 	
 	@Data
